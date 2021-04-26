41c065969b517ce066cb8f23fc538048c0943038
Manthan Patel <patelmanthan1806@gmail.com>
2021-04-26 09:33:24 +0530

    firts

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./css/style.css";
+import Game from "./components/Game";
+
+// ========================================
+
+ReactDOM.render(<Game />, document.getElementById("root"));
